e9ac794580661edae8f31c034ae4f71bdb6d2fb4
Vincent (Anapher) <anapher@protonmail.com>
2018-05-13 22:01:23 +0200

    Add projects

---
+++ src/Orcus.Administration.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Orcus.Administration.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Orcus.Modules.Api/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Orcus.Modules.Api
+{
+    public class Class1
+    {
+    }
+}
