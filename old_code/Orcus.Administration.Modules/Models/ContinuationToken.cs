2419f4b56374b80d1a87c58a11d84ed477379655
Vincent (Anapher) <anapher@protonmail.com>
2018-08-17 21:34:58 +0200

    Remove old_code (it's working, no need for that anymore)

--- old_code/Orcus.Administration.Modules/Models/ContinuationToken.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Orcus.Administration.Modules.Models
-{
-    /// <summary>
-    /// Feed specific data describing current position to continue search from.
-    /// It's opaque to external consumer.
-    /// </summary>
-    public class ContinuationToken { }
-}
