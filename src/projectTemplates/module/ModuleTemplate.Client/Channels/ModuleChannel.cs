ecfa7dea902a45f9c790354850aa1ec9b260702b
Vincent (Anapher) <anapher@protonmail.com>
2019-04-07 13:22:15 +0200

    Update module template

--- src/projectTemplates/module/ModuleTemplate.Client/Channels/ModuleChannel.cs
+++ src/projectTemplates/module/ModuleTemplate.Client/Channels/ModuleChannel.cs
@@ -1,11 +1,11 @@
-using ModuleTemplate.Shared.Channels;
-using Maze.ControllerExtensions;
-using Maze.Modules.Api.Routing;
+//using ModuleTemplate.Shared.Channels;
+//using Maze.ControllerExtensions;
+//using Maze.Modules.Api.Routing;
 
-namespace ModuleTemplate.Client.Channels
-{
-    [Route("testChannel")]
-    public class ModuleNamePlaceholderChannel : CallTransmissionChannel<IChannel>, IChannel
-    {
-    }
-}
+//namespace ModuleTemplate.Client.Channels
+//{
+//    [Route("testChannel")]
+//    public class ModuleNamePlaceholderChannel : CallTransmissionChannel<IChannel>, IChannel
+//    {
+//    }
+//}
